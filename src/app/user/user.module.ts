ce12ce9196d351c387384fa6b488b7a8f8c04385
Wu Ming-Hsueh <ri3567@gmail.com>
2018-05-21 15:24:58 +0800

    login 表單驗證

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [LoginComponent, RegisterComponent]
 })
